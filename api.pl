3e24b93a58e6ebb79c310b330410191938cc3626
root <y-fujita@ist.osaka-u.ac.jp>
2014-01-21 14:16:21 -0800

    update

--- api.pl
+++ api.pl
@@ -1,43 +1,74 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use utf8;
 
-my $inputfile = "test_slice.conf";
+my ($slice_file, $conf_file);
 
-# ハッシュの作成
-# my %hash;
+if(@ARGV == 2){
+  $conf_file = $ARGV[0];
+  $slice_file = $ARGV[1];
+}
+if(@ARGV == 3){
+  my $tate = $ARGV[0];
+  my $yoko = $ARGV[1];
+  my $slice_unit = $ARGV[2];
+  my $com1 = "./make_topology " . $tate . " " . $yoko;
+  print $com1 . "\n";
+  system($com1) || die ("ERROR: cannot do ./make_topology");
+  my $com2 = "./make_slicefile " . $tate . " " . $yoko . " " . $slice_unit;
+  print $com2 . "\n";
+  system($com2) || die ("ERROR: cannot do ./make_slicefile");
 
-# inputfileの読み込み
-open(DATAFILE, $inputfile) or die ("ERROR: no inputfile!");
-while(my $line = <DATAFILE>) {
-  # 改行を削除
-  chomp $line;
-  # 正規表現にマッチするかチェックする
-  if(! $line =~ /slice([0-9]){(.*)}/) {
-    # マッチしなければプログラムを終了する 
-    exit(0);
+  $conf_file = "test_topology.conf";
+  $slice_file = "test_slice.conf";
+}
+if(@ARGV == 0){
+  print "usage: perl api.pl <topology_file> <slice_file>\n";
+  print "usage: perl api.pl <height_nodenum> <width_nodenum> <slice_unitnum>\n";
+  exit(0);
+}
+
+  # slicefileの読み込み
+  open(DATAFILE, $slice_file) or die ("ERROR: no input_file!");
+  while(my $line = <DATAFILE>) {
+    # 改行を削除
+    chomp $line;
+    # 正規表現にマッチするかチェックする
+    if(! $line =~ /slice([0-9]){[0-9](.*)}/) {
+      # マッチしなければプログラムを終了する 
+      print "slice_file is wrong.\n";
+      print $line . "\n";
+      exit(0);
+    }
   }
-}
-close(DATAFILE);
+  close(DATAFILE);
 
+  # slice.rbの作成
+  open(INITFILE, "slice_init.pl") or die ("ERROR: no slice_init!");
+  open(RUBYFILE, ">slice.rb") or die ("ERROR: cannot open slice.rb!");
+  while(my $line1 = <INITFILE>) {
+    if($line1 =~ /.*test_slice\.conf.*/) {
+      print RUBYFILE "    \@input_file = \"" . $slice_file . "\"\n" ;
+    }
+    else {
+      print RUBYFILE $line1;
+    }
+  }
 
-# 外部コマンドの実行 
- my $command0 = "sudo trema run \"./routing-switch.rb graphviz /tmp/topology.png\" -c test_topology.conf |";
-#my $command0 = "sudo trema run ./routing-switch.rb -c test_topology.conf |";
-print "command: " . $command0 . "\n";
-open(IN, $command0);
-while(my $line0 = <IN>) {
-  print $line0;
-}
-close(IN);
+  # 外部コマンドの実行 
+  $SIG{PIPE} = 'IGNORE';
+  my $command0 = "sudo trema run ./routing-switch.rb -c " . $conf_file . " |";
+  print "command: " . $command0 . "\n";
+  open(IN, $command0) || die "can't fork: $!";
+  while(<IN>) {
+    print;
+  }
+  close(IN) || die "command end!";
 
-my $command1 = "sudo trema killall |";
-print "command: " . $command1 . "\n";
-open(IN, $command1);
-while(my $line1 = <IN>) {
-  print $line1;
-}
-close(IN);
+  my $command1 = "sudo trema killall";
+  system($command1);
+
 
 # my $outputfile = "slice1.conf";
 # outputfileへの書き込み
@@ -48,3 +79,4 @@ close(IN);
 # print 
 # print OUT $string;
 # close(OUT);
+
